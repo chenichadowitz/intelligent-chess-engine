7e170ef535a955615c8f23a07cd1c289f5de4c05
jonathan.goldin@gmail.com <jonathan.goldin@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-02-07 20:21:49 +0000

    code for king

--- trunk/src/King.java
+++ trunk/src/King.java
@@ -7,6 +7,18 @@ public class King extends piece{
 		value = 0;
 	}
 	void generateMoves(){
+		boolean[] status;
+		int[] square = new int[2];
+		for(int column = -1; column <=1; column++){
+			for(int row = -1; row <= 1; row++){
+				square[0] = position[0] + column;
+				square[1] = position[1] + row;
+				status = currentBoard.statusOfSquare(square);
+				if ((status[0] && status[1] == !color) || (!status[0] && status[1])) {
+					moves.add(square);
+				}
+			}
+		}
 		
 	}
 }
