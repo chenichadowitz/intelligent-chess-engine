3cdbea39b5a8214d7d33429870f11229c297d51a
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-02-22 19:46:43 +0000

    -made abstract player vars protected, not private -made player not abstract to allow for creating dummy players

--- src/ice/Player.java
+++ src/ice/Player.java
@@ -1,7 +1,7 @@
 package ice;
 
-public abstract class Player {
-	private boolean color;
-	private boolean checkStatus = false;
+public class Player {
+	protected boolean color;
+	protected boolean checkStatus = false;
 	public boolean getCheckStatus(){ return checkStatus; }
 }
