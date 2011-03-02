cb55e0575f697ef00b71f9c1d26bd0f8052f15b2
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-02 19:51:10 +0000

    - added beginning of gui - Board.display now returns string until gui is sorted out

--- src/ice/Driver.java
+++ src/ice/Driver.java
@@ -1,10 +1,11 @@
 package ice;
 
+import java.util.Scanner;
+
 public class Driver {
 
-	/**
-	 * @param args
-	 */
+	private static Scanner genericScanner = new Scanner(System.in);
+	
 	public static void debug(String action){
 		System.out.println(action);
 		//possibly write to file
@@ -14,12 +15,27 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		HumanPlayer white = new HumanPlayer(true);
+		System.out.print("White player name: ");
+		if(genericScanner.hasNext()){
+			white.setName(genericScanner.next());
+		}
+		System.out.println();
 		HumanPlayer black = new HumanPlayer(false);
+		System.out.print("Black player name: ");
+		if(genericScanner.hasNext()){
+			black.setName(genericScanner.next());
+		}
+		System.out.println();
 		HumanPlayer current;
 		gameBoard gb = new gameBoard(white, black);
 		gb.setUpBoard();
 		boolean moveResult;
-		gb.display();
+		/*
+		BoardFrame frame = new BoardFrame("TEst title");
+		frame.setSize(800,600);
+		frame.setText(gb.display());
+		*/
+		System.out.println(gb.display());
 		while(true){
 			if(gb.getTurn()){
 				current = white;
@@ -39,9 +55,13 @@ public class Driver {
 					for(int i=0;i<4;i++) move[i]--;
 					moveResult = gb.movePiece(move);
 					//System.out.println("MoveResult: "+moveResult);
-				} else { gb.display(); }
+				} else { 
+					//frame.setText(gb.display());
+					System.out.println(gb.display());
+				}
 			}
-			gb.display();
+			//frame.setText(gb.display());
+			System.out.println(gb.display());
 			gb.switchTurn();
 		}
 		
