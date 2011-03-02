303568cdae89e29eb2dbb9d3c9050879439914f5
jonathan.goldin@gmail.com <jonathan.goldin@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-02 21:45:48 +0000

    test

--- trunk/src/ice/gameBoard.java
+++ trunk/src/ice/gameBoard.java
@@ -12,9 +12,17 @@ public class gameBoard extends Board{
 		pieces.add(new King  (true, 4,0 ,this));
 		pieces.add(new Knight(true, 1,0 ,this));
 		pieces.add(new Knight(true, 6,0 ,this));
-		pieces.add(new King  (false, 4,7 ,this));
+		pieces.add(new Rook  (true, 7,0 ,this));
+		//black pieces
+		pieces.add(new Rook  (false, 0,7 ,this));
 		pieces.add(new Knight(false, 1,7 ,this));
+		pieces.add(new Bishop(false, 2,7 ,this));
+		pieces.add(new Queen (false, 3,7 ,this));
+		pieces.add(new King  (false, 4,7 ,this));
+		pieces.add(new Bishop(false, 5,7 ,this));
 		pieces.add(new Knight(false, 6,7 ,this));
+		pieces.add(new Rook  (false, 7,7 ,this));
+		
 		buildBoardState();
 		Driver.debug("board set up",1);
 		//only for TESTING
