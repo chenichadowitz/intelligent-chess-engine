daa73e99d81bf63a1d9354d31f242b079c968654
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-04 02:06:18 +0000

    - more xboard changes, will now make 7 half-moves when pitted against itself

--- trunk/src/ice/Driver.java
+++ trunk/src/ice/Driver.java
@@ -73,7 +73,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		if(args.length > 0 && args[0].equals("xboard")){
-			Xboard.run();
+			Xboard.run(args);
 		} else {
 			humanVShuman();
 		}
--- trunk/src/ice/Xboard.java
+++ trunk/src/ice/Xboard.java
@@ -10,8 +10,7 @@ import java.util.regex.Pattern;
 
 public class Xboard {
 	
-	private static BufferedReader reader =
-		  new BufferedReader(new InputStreamReader(System.in));	
+	private static BufferedReader reader;
 	private static FileWriter writer;
 	
 	private static ArrayList<String> AcceptedFeatures = new ArrayList<String>(); 
@@ -20,53 +19,70 @@ public class Xboard {
 	private static ArrayList<String> MovesMade = new ArrayList<String>();
 
 	private static boolean quit = false;
+	private static int moveCounter = 0;
+	private static boolean go = false;
 	
 	private static Pattern movePat = Pattern.compile("[a-h][1-8][a-h][1-8]");
 	
 	// Entry point for an XBoard run....
-	public static void run(){
+	public static void run(String[] args){
 		try{
-			writer = new FileWriter("output.log", true);
-		} catch(IOException e){
-			System.exit(-1);
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			log("args length: ", ""+args.length);
+			if(args.length > 1){
+				writer = new FileWriter("/home/cheni/"+args[1], true);
+			} else {
+				writer = new FileWriter("/home/cheni/xboardOutput.log", true);
+			}
+		} catch(Exception e){
+			//System.exit(-1);
 		}
 		String input;
-		while(!quit){
+		while(true){
 			input = getInput();
 			parseInput(input);
 		}
+		/*
+		try{
+			writer.close();
+		} catch(Exception e){}*/
 	}
 	
 	private static String getInput(){
-		//while(!reader.()()){
-		//}
 		try{
 			return reader.readLine();
-		} catch(IOException e){
+		} catch(Exception e){
 			log("WARNING","FAILED READING LINE");
-			System.exit(1);
-			return null;
+			//System.exit(1);
 		}
+		return null;
 	}
 	
 	private static void parseInput(String input){
 		String response = "";
 		Matcher matchMovePat = movePat.matcher(input);
 		
-		if(input.equals(""))
+		if(input.equals("quit")){
+			quit = true;
+			return;
+		}
 		
 		if(input.equals("xboard")){
 			//Let the interface know that we're here and listening
 			response = "\n";
 		}
 		if(input.equals("protover 2")){
-			response = "feature usermove=1 done=1";
+			response = "feature usermove=1 sigint=0 sigterm=0 nps=0 done=1";
 		}
 		if(input.startsWith("accepted ")){
 			AcceptedFeatures.add(input.substring(9));
 		}
 		if(input.startsWith("rejected ")){
 			RejectedFeatures.add(input.substring(9));
+		}
+		if(input.equals("white")){
+			go = true;
+			response = "move h2h4";
 		}
 		if(input.startsWith("usermove ") || matchMovePat.matches()){
 			String move;
@@ -77,36 +93,51 @@ public class Xboard {
 			}
 			MovesReceived.add(move);
 			//Opponent made move "move"
-			
-			int a = (int)(8*Math.random() + 1);
-			char c = (char)(a+96);
-			response = "move " + c + 7 + c + 5;
-			while(contains(response, MovesMade)){
-				a = (int)(8*Math.random() + 1);
-				c = (char)(a+96);
-				response = "move " + c + 7 + c + 5;
+			if(!go){
+				if(moveCounter == 0){
+					response = "move a7a5";
+				} else if(moveCounter == 1){
+					response = "move b7b5";
+				} else {
+					response = "move c7c5";
+				}
+			} else {
+				if(moveCounter == 0){
+					response = "move a2a4";
+				} else if(moveCounter == 1){
+					response = "move b2b4";
+				} else {
+					response = "move c2c4";
+				}
 			}
+			
+			
+			moveCounter++;
+			
 			MovesMade.add(response);
 		}
 		log(input, response);
-		System.out.println(response);
-		System.out.flush();
+		System.out.println("tellall "+response);
+		if(!response.equals("")){
+			System.out.println(response);
+		}
+		//System.out.flush();
 	}
-	
+	/*
 	private static boolean contains(String needle, ArrayList<String> haystack){
 		for(String s : haystack){
 			if(needle.equals(s)) return true;
 		}
 		return false;			
 	}
-	
+	*/
 	private static void log(String input, String response){
 		try{
 			writer.write(input + " : \"" + response + "\"");
 			writer.write("\n");
 			writer.flush();
-		} catch(IOException e){
-			System.exit(1);
+		} catch(Exception e){
+			//System.exit(1);
 		}
 	}
 	
