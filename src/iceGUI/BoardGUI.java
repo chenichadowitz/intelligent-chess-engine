34f0749809f1771233232491f40f426f0771a4c1
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-09 01:31:56 +0000

    - made iceGUI package, started gui more

--- src/iceGUI/BoardGUI.java
+++ src/iceGUI/BoardGUI.java
@@ -1,11 +1,12 @@
-package ice;
+package iceGUI;
+
 
 import javax.swing.*;
 
 public class BoardGUI{
 
 	public static void main(String[] args){
-		BoardFrame frame = new BoardFrame("TEst title");
+		BoardFrame frame = new BoardFrame("Test title");
 		frame.setSize(800,600);
 		frame.setText("changed");
 
