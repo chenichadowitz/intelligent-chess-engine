c8a524df3803c61bf8c5bb105682696e1aec9cf8
jonathan.goldin@gmail.com <jonathan.goldin@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-13 01:29:21 +0000

    bug fixing, black castling still results in error

--- trunk/src/ice/King.java
+++ trunk/src/ice/King.java
@@ -1,9 +1,7 @@
 package ice;
 
-import java.util.ArrayList;
 
 public class King extends Piece{
-	protected ArrayList<Integer[]> listeningSquares = new ArrayList<Integer[]>(); //[[x1,y1][x2,y2]...]
 	public King(boolean player, int xwhere, int ywhere, Board onWhat){
 		color = player;
 		position[0] = xwhere;
@@ -15,7 +13,6 @@ public class King extends Piece{
 	}
 	public void generateMoves(){
 		super.generateMoves();
-		listeningSquares =  new ArrayList<Integer[]>();
 		processSquare(position[0] -1,position[1] -1);
 		processSquare(position[0] -1,position[1]   );
 		processSquare(position[0] -1,position[1] +1);
@@ -36,9 +33,9 @@ public class King extends Piece{
 					int[]     square = {position[0] +2*delta, position[1]};
 					boolean[] status = currentBoard.statusOfSquare(square);
 					if (!status[0] && status[1]){
-						possibleMoves.add(new Move(currentBoard,this,square));
+						possibleMoves.add(new Move(currentBoard,this,square,true));
 					} else {
-						listeningSquares.add(new Integer[] {square[0],square[1]});
+						possibleMoves.add(new Move(currentBoard,this,square,false));
 					}
 				}
 			}
@@ -52,37 +49,20 @@ public class King extends Piece{
 					int[]     square = {position[0] +2*delta, position[1]};
 					boolean[] status = currentBoard.statusOfSquare(square);
 					if (!status[0] && status[1]){
-						possibleMoves.add(new Move(currentBoard,this,square));
+						possibleMoves.add(new Move(currentBoard,this,square,true));
 					} else {
-						listeningSquares.add(new Integer[] {square[0],square[1]});
+						possibleMoves.add(new Move(currentBoard,this,square,false));
 					}
 				}
 			}
 		}
 	}
-	@SuppressWarnings("unchecked")
 	public Piece clone(){
 		King newPiece = new King(color,position[0],position[1],currentBoard);
 		for(Move moveCloner: possibleMoves){
 			newPiece.possibleMoves.add(moveCloner.clone());
 		}
-		newPiece.listeningSquares = (ArrayList<Integer[]>) listeningSquares.clone();
+		newPiece.castle = castle;
 		return newPiece;
-	}
-	public void addToBoardState(){
-		super.addToBoardState();
-		if(castle){
-			for(Integer[] square: listeningSquares){
-				currentBoard.boardState[square[0]][square[1]].add(this);
-			}
-		}
-	}
-	public void removeFromBoardState(){
-		super.removeFromBoardState();
-		if(castle){
-			for(Integer[] square: listeningSquares){
-				currentBoard.boardState[square[0]][square[1]].remove(this);
-			}
-		}
 	}
 }
