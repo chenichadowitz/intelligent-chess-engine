21a703ad257fce41ebb29199d247b8c8c62be962
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-03-21 23:52:24 +0000

    - added helper comments for BoardState methods

--- src/gameLogic/BoardState.java
+++ src/gameLogic/BoardState.java
@@ -10,28 +10,58 @@ public class BoardState {
 
 	private HashMap<int[], LinkedList<Piece>> map;
 
+	/**
+	 * Creates a new BoardState object
+	 */
 	public BoardState(){
 		 map = new HashMap<int[], LinkedList<Piece>>(64);
 	}
 	
+	/**
+	 * Returns a LinkedList<Piece> for a given square
+	 * @param square the square to look for
+	 * @return LinkedList<Piece> for the given square
+	 */
 	public LinkedList<Piece> getPieceList(int[] square){
 		return map.get(square);
 	}
 	
+	/**
+	 * Returns a LinkedList<Piece> for a given square
+	 * @param x x-coordinate of the square to look for
+	 * @param y y-coordinate of the square to look for
+	 * @return LinkedList<Piece> for the given square
+	 */
 	public LinkedList<Piece> getPieceList(int x, int y){
 		int[] square = {x, y};
 		return this.getPieceList(square);
 	}
 		
+	/**
+	 * Add a Piece to the given square's list
+	 * @param square square to add a Piece to
+	 * @param p Piece to add to the given square
+	 */
 	public void addPiece(int[] square, Piece p){
 		map.get(square).add(p);
 	}
 	
+	/**
+	 * Add a Piece to the given square's list
+	 * @param x x-coordinate of the square to add to
+	 * @param y y-coordinate of the square to add to
+	 * @param p Piece to add to the given square
+	 */
 	public void addPiece(int x, int y, Piece p){
 		int[] square = {x,y};
 		this.addPiece(square, p);
 	}
 	
+	/**
+	 * Add the given move's Piece to the given square
+	 * @param square square to add the move's Piece to
+	 * @param action the move whose Piece to add
+	 */
 	public void addMove(int[] square, Listener action){
 		LinkedList<Piece> pieces = this.getPieceList(square);
 		if(pieces.contains(action.movingPiece)){
@@ -40,10 +70,28 @@ public class BoardState {
 		map.get(square).add(action.movingPiece);
 	}
 	
+	/**
+	 * Add the given move's Piece to the given square
+	 * @param x x-coordinate of the square to add the move's Piece to
+	 * @param y y-coordinate of the square to add the move's Piece to
+	 * @param action the move whose Piece to add
+	 */
+	public void addMove(int x, int y, Listener action){
+		int[] square = {x,y};
+		this.addMove(square, action);
+	}
+	
+	/**
+	 * Clear the BoardState and re-initialize it to empty
+	 */
 	public void clearBoardState(){
 		map = new HashMap<int[], LinkedList<Piece>>();
 	}
 	
+	/**
+	 * Update the given squares to reflect any changes that have been made
+	 * @param squares the squares to update (any number of 2-element integer arrays)
+	 */
 	public void update(int[]... squares){
 		ArrayList<Piece> piecesToUpdate = new ArrayList<Piece>();
 		for(int[] square : squares){
