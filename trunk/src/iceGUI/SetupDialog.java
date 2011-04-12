d07f645728ec70e49af69cc251695bcb4ffc9865
pheonix7117@gmail.com <pheonix7117@gmail.com@ce8c7692-6851-7df3-0d26-6e4082f8c334>
2011-04-12 02:00:27 +0000

    - built the setup dialog, just need to connect it to GamePanel now....

--- trunk/src/iceGUI/SetupDialog.java
+++ trunk/src/iceGUI/SetupDialog.java
@@ -2,42 +2,53 @@ package iceGUI;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import net.miginfocom.swing.MigLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
 import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-import javax.swing.JSlider;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.security.InvalidParameterException;
 
-public class SetupDialog extends JDialog implements ChangeListener, ActionListener {
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+
+import newerGameLogic.ComputerPlayer;
+import newerGameLogic.HumanPlayer;
+import newerGameLogic.Player;
+import newerGameLogic.WBColor;
+
+public class SetupDialog extends JDialog  implements ChangeListener, ActionListener{
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtWhiteName;
-	private JTextField txtBlackName;
-	private JSlider sliderB;
+	private JTextField txtPlayerW;
+	private JTextField txtPlayerB;
+	private JLabel lblDifficultyW;
+	private JLabel lblDifficultyB;
+	private JLabel lblDiffValW;
+	private JLabel lblDiffValB;
+	private JRadioButton rbHumanW;
+	private JRadioButton rbHumanB;
+	private JLabel lblHumanW;
+	private JLabel lblHumanB;
+	private JRadioButton rbComputerW;
+	private JRadioButton rbComputerB;
 	private JSlider sliderW;
-	private JTextField blackCompLvl;
-	private JTextField whiteCompLvl;
-	private JLabel lblWhiteName;
-	private JLabel lblWhiteDifficulty;
-	private JRadioButton rbWhiteComputer;
-	private JRadioButton rbWhiteHuman;
-	private JLabel lblBlackName;
-	private JLabel lblBlackDifficulty;
-	private JRadioButton rbBlackComputer;
-	private JRadioButton rbBlackHuman;
+	private JSlider sliderB;
+	private JButton okButton;
+	private JButton cancelButton;
+	private GamePanel parent;
 
 	/**
 	 * Launch the application.
@@ -52,79 +63,162 @@ public class SetupDialog extends JDialog implements ChangeListener, ActionListen
 		}
 	}
 
+	public SetupDialog(GamePanel gp){
+		this();
+		parent = gp;
+	}
+	
 	/**
 	 * Create the dialog.
 	 */
 	public SetupDialog() {
-		setAlwaysOnTop(true);
-		setModal(true);
-		setTitle("Setup Game");
 		setBounds(100, 100, 450, 300);
+		setTitle("Setup Game");
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new MigLayout("", "[][][][grow]", "[][][][]"));
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
 		{
 			JLabel lblWhite = new JLabel("White:");
-			contentPanel.add(lblWhite, "cell 0 0");
+			GridBagConstraints gbc_lblWhite = new GridBagConstraints();
+			gbc_lblWhite.insets = new Insets(0, 0, 5, 5);
+			gbc_lblWhite.gridx = 0;
+			gbc_lblWhite.gridy = 0;
+			contentPanel.add(lblWhite, gbc_lblWhite);
 		}
 		{
-			rbWhiteHuman = new JRadioButton("Human");
-			rbWhiteHuman.setSelected(true);
-			contentPanel.add(rbWhiteHuman, "cell 1 0");
+			rbHumanW = new JRadioButton("Human");
+			rbHumanW.setSelected(true);
+			rbHumanW.addActionListener(this);
+			GridBagConstraints gbc_rbHumanW = new GridBagConstraints();
+			gbc_rbHumanW.insets = new Insets(0, 0, 5, 5);
+			gbc_rbHumanW.gridx = 1;
+			gbc_rbHumanW.gridy = 0;
+			contentPanel.add(rbHumanW, gbc_rbHumanW);
 		}
 		{
-			lblWhiteName = new JLabel("Name:");
-			contentPanel.add(lblWhiteName, "cell 2 0,alignx trailing");
+			lblHumanW = new JLabel("Name:");
+			GridBagConstraints gbc_lblHumanW = new GridBagConstraints();
+			gbc_lblHumanW.insets = new Insets(0, 0, 5, 5);
+			gbc_lblHumanW.gridx = 2;
+			gbc_lblHumanW.gridy = 0;
+			contentPanel.add(lblHumanW, gbc_lblHumanW);
 		}
 		{
-			txtWhiteName = new JTextField();
-			txtWhiteName.setText("Player1");
-			contentPanel.add(txtWhiteName, "cell 3 0,growx");
-			txtWhiteName.setColumns(10);
+			txtPlayerW = new JTextField();
+			txtPlayerW.setText("Player1");
+			GridBagConstraints gbc_txtPlayerW = new GridBagConstraints();
+			gbc_txtPlayerW.insets = new Insets(0, 0, 5, 0);
+			gbc_txtPlayerW.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtPlayerW.gridx = 4;
+			gbc_txtPlayerW.gridy = 0;
+			contentPanel.add(txtPlayerW, gbc_txtPlayerW);
+			txtPlayerW.setColumns(10);
 		}
 		{
-			rbWhiteComputer = new JRadioButton("Computer");
-			contentPanel.add(rbWhiteComputer, "cell 1 1");
+			rbComputerW = new JRadioButton("Computer");
+			rbComputerW.addActionListener(this);
+			GridBagConstraints gbc_rbComputerW = new GridBagConstraints();
+			gbc_rbComputerW.insets = new Insets(0, 0, 5, 5);
+			gbc_rbComputerW.gridx = 1;
+			gbc_rbComputerW.gridy = 1;
+			contentPanel.add(rbComputerW, gbc_rbComputerW);
 		}
 		{
-			lblWhiteDifficulty = new JLabel("Difficulty:");
-			contentPanel.add(lblWhiteDifficulty, "cell 2 1");
+			lblDifficultyW = new JLabel("Difficulty:");
+			GridBagConstraints gbc_lblDifficultyW = new GridBagConstraints();
+			gbc_lblDifficultyW.insets = new Insets(0, 0, 5, 5);
+			gbc_lblDifficultyW.gridx = 2;
+			gbc_lblDifficultyW.gridy = 1;
+			contentPanel.add(lblDifficultyW, gbc_lblDifficultyW);
+		}
+		{
+			lblDiffValW = new JLabel("5");
+			GridBagConstraints gbc_lblDiffValW = new GridBagConstraints();
+			gbc_lblDiffValW.insets = new Insets(0, 0, 5, 5);
+			gbc_lblDiffValW.gridx = 3;
+			gbc_lblDiffValW.gridy = 1;
+			contentPanel.add(lblDiffValW, gbc_lblDiffValW);
 		}
 		{
 			sliderW = new JSlider();
-			sliderW.setMaximum(10);
 			sliderW.setValue(5);
 			sliderW.setSnapToTicks(true);
+			sliderW.setMaximum(10);
 			sliderW.addChangeListener(this);
-			contentPanel.add(sliderW, "flowx,cell 3 1");
+			GridBagConstraints gbc_sliderW = new GridBagConstraints();
+			gbc_sliderW.fill = GridBagConstraints.HORIZONTAL;
+			gbc_sliderW.insets = new Insets(0, 0, 5, 0);
+			gbc_sliderW.gridx = 4;
+			gbc_sliderW.gridy = 1;
+			contentPanel.add(sliderW, gbc_sliderW);
 		}
 		{
 			JLabel lblBlack = new JLabel("Black:");
-			contentPanel.add(lblBlack, "cell 0 2");
+			GridBagConstraints gbc_lblBlack = new GridBagConstraints();
+			gbc_lblBlack.insets = new Insets(0, 0, 5, 5);
+			gbc_lblBlack.gridx = 0;
+			gbc_lblBlack.gridy = 2;
+			contentPanel.add(lblBlack, gbc_lblBlack);
 		}
 		{
-			rbBlackHuman = new JRadioButton("Human");
-			rbBlackHuman.setSelected(true);
-			contentPanel.add(rbBlackHuman, "cell 1 2");
+			rbHumanB = new JRadioButton("Human");
+			rbHumanB.setSelected(true);
+			rbHumanB.addActionListener(this);
+			GridBagConstraints gbc_rbHumanB = new GridBagConstraints();
+			gbc_rbHumanB.insets = new Insets(0, 0, 5, 5);
+			gbc_rbHumanB.gridx = 1;
+			gbc_rbHumanB.gridy = 2;
+			contentPanel.add(rbHumanB, gbc_rbHumanB);
 		}
 		{
-			lblBlackName = new JLabel("Name:");
-			contentPanel.add(lblBlackName, "cell 2 2,alignx trailing");
+			lblHumanB = new JLabel("Name:");
+			GridBagConstraints gbc_lblHumanB = new GridBagConstraints();
+			gbc_lblHumanB.insets = new Insets(0, 0, 5, 5);
+			gbc_lblHumanB.gridx = 2;
+			gbc_lblHumanB.gridy = 2;
+			contentPanel.add(lblHumanB, gbc_lblHumanB);
 		}
 		{
-			txtBlackName = new JTextField();
-			txtBlackName.setText("Player2");
-			contentPanel.add(txtBlackName, "cell 3 2,growx");
-			txtBlackName.setColumns(10);
+			txtPlayerB = new JTextField();
+			txtPlayerB.setText("Player2");
+			GridBagConstraints gbc_txtPlayerB = new GridBagConstraints();
+			gbc_txtPlayerB.insets = new Insets(0, 0, 5, 0);
+			gbc_txtPlayerB.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtPlayerB.gridx = 4;
+			gbc_txtPlayerB.gridy = 2;
+			contentPanel.add(txtPlayerB, gbc_txtPlayerB);
+			txtPlayerB.setColumns(10);
 		}
 		{
-			rbBlackComputer = new JRadioButton("Computer");
-			contentPanel.add(rbBlackComputer, "cell 1 3");
+			rbComputerB = new JRadioButton("Computer");
+			rbComputerB.addActionListener(this);
+			GridBagConstraints gbc_rbComputerB = new GridBagConstraints();
+			gbc_rbComputerB.insets = new Insets(0, 0, 0, 5);
+			gbc_rbComputerB.gridx = 1;
+			gbc_rbComputerB.gridy = 3;
+			contentPanel.add(rbComputerB, gbc_rbComputerB);
 		}
 		{
-			lblBlackDifficulty = new JLabel("Difficulty:");
-			contentPanel.add(lblBlackDifficulty, "cell 2 3");
+			lblDifficultyB = new JLabel("Difficulty:");
+			GridBagConstraints gbc_lblDifficultyB = new GridBagConstraints();
+			gbc_lblDifficultyB.insets = new Insets(0, 0, 0, 5);
+			gbc_lblDifficultyB.gridx = 2;
+			gbc_lblDifficultyB.gridy = 3;
+			contentPanel.add(lblDifficultyB, gbc_lblDifficultyB);
+		}
+		{
+			lblDiffValB = new JLabel("5");
+			GridBagConstraints gbc_lblDiffValB = new GridBagConstraints();
+			gbc_lblDiffValB.insets = new Insets(0, 0, 0, 5);
+			gbc_lblDiffValB.gridx = 3;
+			gbc_lblDiffValB.gridy = 3;
+			contentPanel.add(lblDiffValB, gbc_lblDiffValB);
 		}
 		{
 			sliderB = new JSlider();
@@ -132,68 +226,119 @@ public class SetupDialog extends JDialog implements ChangeListener, ActionListen
 			sliderB.setSnapToTicks(true);
 			sliderB.setMaximum(10);
 			sliderB.addChangeListener(this);
-			contentPanel.add(sliderB, "flowx,cell 3 3");
-		}
-		{
-			blackCompLvl = new JTextField("5");
-			blackCompLvl.setEditable(false);
-			contentPanel.add(blackCompLvl, "cell 3 3");
-			blackCompLvl.setColumns(10);
-		}
-		{
-			whiteCompLvl = new JTextField("5");
-			whiteCompLvl.setEditable(false);
-			contentPanel.add(whiteCompLvl, "cell 3 1");
-			whiteCompLvl.setColumns(10);
+			GridBagConstraints gbc_sliderB = new GridBagConstraints();
+			gbc_sliderB.gridx = 4;
+			gbc_sliderB.gridy = 3;
+			contentPanel.add(sliderB, gbc_sliderB);
 		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
+				okButton = new JButton("OK");
 				okButton.setActionCommand("OK");
+				okButton.addActionListener(this);
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
+				cancelButton = new JButton("Cancel");
 				cancelButton.setActionCommand("Cancel");
+				cancelButton.addActionListener(this);
 				buttonPane.add(cancelButton);
 			}
 		}
+		
+		toggleWhiteHuman(true);
+		toggleBlackHuman(true);
+	
 	}
 
-	public void stateChanged(ChangeEvent arg0) {
-		if(sliderW.getValue() != Integer.parseInt(whiteCompLvl.getText()))
-			whiteCompLvl.setText(Integer.toString(sliderW.getValue()));
-		if(sliderB.getValue() != Integer.parseInt(blackCompLvl.getText()))
-			blackCompLvl.setText(Integer.toString(sliderB.getValue()));		
-	}
-
-	private void toggleWhiteHuman(boolean b){
-		lblWhiteName.setVisible(b);
+	private void toggleWhiteHuman(boolean visible){
+		rbHumanW.setSelected(visible);
+		txtPlayerW.setVisible(visible);
+		lblHumanW.setVisible(visible);
+		toggleWhiteComp(!visible);
 	}
 	
-	private void toggleWhiteComp(){
+	private void toggleWhiteComp(boolean visible){
+		rbComputerW.setSelected(visible);
+		lblDifficultyW.setVisible(visible);
+		lblDiffValW.setVisible(visible);
+		sliderW.setVisible(visible);
+	}
+	
+	private void toggleBlackHuman(boolean visible){
+		rbHumanB.setSelected(visible);
+		txtPlayerB.setVisible(visible);
+		lblHumanB.setVisible(visible);
+		toggleBlackComp(!visible);
+	}
+	
+	private void toggleBlackComp(boolean visible){
+		rbComputerB.setSelected(visible);
+		lblDifficultyB.setVisible(visible);
+		lblDiffValB.setVisible(visible);
+		sliderB.setVisible(visible);
+	}
+	
+	private void gatherAndReturnChoices(){
+		Player white = collectPlayerInfo(WBColor.White);
+		Player black = collectPlayerInfo(WBColor.Black);
 		
-	}
-	
-	private void toggleBlackHuman(){
-		
-	}
-	
-	private void toggleBlackComp(){
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(rbWhiteHuman)){
-			lblWhiteName.setVisible(true);
-			sliderW.setVisible(false);
-			
+		if(parent != null){
+			//parent.newGame(white, black);
+		} else {
+			System.out.println(white.getClass().getName() + " " + white.getName());
+			System.out.println(black.getClass().getName() + " " + black.getName());
 		}
-		
 	}
+	
+	private Player collectPlayerInfo(WBColor color){
+		switch(color){
+		case White:
+			if(rbHumanW.isSelected()){
+				Player white = new HumanPlayer(WBColor.White);
+				white.setName(txtPlayerW.getText());
+				return white;
+			} else 
+				return new ComputerPlayer(WBColor.White, sliderW.getValue());
+		case Black:
+			if(rbHumanB.isSelected()){
+				Player black = new HumanPlayer(WBColor.Black);
+				black.setName(txtPlayerB.getText());
+				return black;
+			} else
+				return new ComputerPlayer(WBColor.Black, sliderB.getValue());
+		default:
+			throw new InvalidParameterException("Color enum unrecognized....");
+		}
+	}
+	
+	
+	public void stateChanged(ChangeEvent arg0) {
+		if(sliderW.getValue() != Integer.parseInt(lblDiffValW.getText()))
+			lblDiffValW.setText(Integer.toString(sliderW.getValue()));
+		if(sliderB.getValue() != Integer.parseInt(lblDiffValW.getText()))
+			lblDiffValB.setText(Integer.toString(sliderB.getValue()));				
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(rbHumanW)){
+			toggleWhiteHuman(true);
+		} else if(e.getSource().equals(rbComputerW)){
+			toggleWhiteHuman(false);
+		} else if(e.getSource().equals(rbHumanB)){
+			toggleBlackHuman(true);
+		} else if(e.getSource().equals(rbComputerB)){
+			toggleBlackHuman(false);
+		} else if(e.getSource().equals(okButton)){
+			gatherAndReturnChoices();
+			this.dispose();
+		} else if(e.getSource().equals(cancelButton)){
+			this.dispose();
+		}
+	}
+
 }
